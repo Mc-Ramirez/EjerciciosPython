96692b1666295ceecb85a283d05a9c7e217c4283
Mc-Ramirez <afrmillan@gmail.com>
2025-02-17 22:13:25 +0100

    ejercicios 1, 2 y 3

---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+nombre = "felipe"
+print(nombre)
+print("Hola, mi nombre es", nombre)
+
+# Hola mi nombre es felipe
---
+++ ejercicio3.py
@@ -0,0 +1,22 @@
+import Constantes
+
+# variables
+variable = "felipe"
+print(variable)
+
+valor = 15
+print(valor)
+
+opcion = False
+print(opcion)
+
+print(Constantes.PI)
+
+nombre1 = nombre2 = "andres"
+print(nombre1)
+print(nombre2)
+
+numero1, numero2, numero3 = "tres", 2, "uno"
+print(numero1)
+print(numero2)
+print(numero3)
